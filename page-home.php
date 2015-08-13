4b2ad15ee3263c4fc7f449543e832b0ac999fe79
Robin Hamill <hamillrobin@gmail.com>
2015-08-13 14:19:25 -0400

    Instagram works now

--- page-home.php
+++ page-home.php
@@ -15,11 +15,11 @@ get_header();  ?>
       <h2><?php the_title(); ?></h2>
       <?php the_content(); ?>
 
-			<?php the_field('instagram'); ?>
     <?php endwhile; // end the loop?>
 
 
   </div> <!-- /.container -->
+			<div class="instagram-container"><?php the_field('instagram'); ?></div>
 </div> <!-- /.main -->
 
 <?php get_footer(); ?>
